56986e7461707f22c3f9c68b9ff3690540044de2
luish000 <luish000.lh@gmail.com>
2016-03-01 15:40:10 -0430

    Updated to 1.0.3
    - Added bindToApp method

--- index.js
+++ index.js
@@ -4,22 +4,62 @@ let path = require('path')
 let Quark = require('proton-quark')
 let _ = require('lodash')
 
-module.exports = class ServicesQuark extends Quark {
+/**
+ * @class ServicesQuark
+ * @classdesc This quark is for instance services
+ * @author Luis Hernandez
+ */
+class ServicesQuark extends Quark {
 
   constructor(proton) {
     super(proton)
   }
 
+  /**
+   * @override
+   * @method configure
+   * @description Ask if the proton.app.services object exist, if not exist
+   * the method create the proton.app.services object
+   * @author Luis Hernandez
+   */
   configure() {
     if (!this.proton.app.services)
       this.proton.app.services = {}
-    return true
   }
 
+  /**
+   * @override
+   * @method initialize
+   * @description instance all models of the app
+   * @author Luis Hernandez
+   */
   initialize() {
-    let servicesPath = path.join(this.proton.app.path, '/services')
-    let services = require('require-all')(servicesPath)
-    _.forEach(services, Service => new Service(this.proton))
+    _.forEach(this._services, (Service, fileName) => {
+      const service = new Service(this.proton)
+      service.fileName = fileName
+      service.expose(service)
+      this.bindToApp(service)
+      return service
+    })
+  }
+
+  bindToApp(...args) {
+    const service = args[0]
+    this.proton.app.services[service.name] = service
+  }
+
+  /**
+   * @method services
+   * @description This method get the export value of each service present
+   * in the services folder
+   * @author Luis Hernandez
+   * @return {Array} - All services exported values as an array
+   */
+  get _services() {
+    const modelsPath = path.join(this.proton.app.path, '/models')
+    return require('require-all')(modelsPath)
   }
 
 }
+
+module.exports = ServicesQuark
